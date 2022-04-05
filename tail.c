d0a893d378f9f26ae26c6d2d98a0949c78899c56
Samuel <samuelstolko@gmail.com>
2022-04-06 00:26:11 +0200

    done little work

--- tail.c
+++ tail.c
@@ -9,3 +9,34 @@
  * 
  */
 
+#include <unistd.h> 
+#include <getopt.h> 
+#include <stdlib.h> 
+#include <stdio.h>
+
+
+int main(int argc, char ** argv)
+{
+    int option = getopt(argc, argv, "n");
+    unsigned long req_lines;
+    switch (option)
+    {
+    case 'n':
+        //TODO check if number and then assign correctly
+         req_lines = strtoul(argv[optind], NULL, 10);
+        printf("%lu\n", req_lines);
+        break;
+
+    case -1:
+        req_lines = 10;
+        break;
+
+    case '?':
+        //default error msg is written by getopt
+        exit(EXIT_FAILURE);
+        break;
+    }
+
+
+    return 0;
+}
