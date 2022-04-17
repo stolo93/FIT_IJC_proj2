2453453c663fa1b087cdb576e923947c641b3577
Samuel <samuelstolko@gmail.com>
2022-04-17 21:48:12 +0200

    htab erase module

---
+++ part_htab/htab_erase.c
@@ -0,0 +1,49 @@
+/**
+ * @file htab_init.c
+ * @author Samuel Stolarik  (xstola03)
+ * @brief 2nd part of DU2 IJC
+ * 
+ * @date 2022-04-10
+ * 
+ */
+#include "htab.h"
+#include "p_htab.h"
+
+#include <stdbool.h>
+
+bool htab_erase(htab_t * t, htab_key_t key)
+{
+    size_t index = htab_hash_function(key) % t -> arr_size;
+    htab_item_t * cur = t -> ptr[index];
+    htab_item_t * prev = NULL;
+
+    bool deleted = false;
+
+    while (cur != NULL)
+    {
+        if (strcmp(key, cur -> pair.key) == 0){
+
+            htab_item_t * tmp = cur -> next;
+            free(cur);
+
+            if (prev == NULL){ //in case the deleted item wasn't the first item in the list
+                t -> ptr[index] = tmp;
+            }
+            else {
+                prev -> next = tmp;
+            }
+            
+            t -> size -= 1; //number of pairs in the htab
+            deleted = true;
+            break;
+        }
+        prev = cur;
+        cur = cur -> next;
+    }
+    
+    if (t -> size / t -> arr_size < AVG_LEN_MIN){
+        htab_resize(t, t -> arr_size / 2);
+    }
+
+    return deleted;
+}
