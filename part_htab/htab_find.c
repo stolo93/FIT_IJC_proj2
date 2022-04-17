0bceecd7f641312daa9965240e1803556ec38e8e
Samuel <samuelstolko@gmail.com>
2022-04-17 11:32:05 +0200

    added find function

---
+++ part_htab/htab_find.c
@@ -0,0 +1,42 @@
+/**
+ * @file htab_init.c
+ * @author Samuel Stolarik  (xstola03)
+ * @brief 2nd part of DU2 IJC
+ * 
+ * @date 2022-04-10
+ * 
+ */
+#include "htab.h"
+#include "p_htab.h"
+#include <string.h>
+
+
+htab_pair_t * htab_find(htab_t * t, htab_key_t key)
+{
+    if (t == NULL)
+        return NULL;
+    
+    if (key == NULL)
+        return NULL;
+
+    htab_item_t * cur_item;
+    
+    for (int i = 0; i < t -> arr_size; i++)
+    {
+        cur_item = t->ptr[i];
+
+        //walk the linked list starting at cur_item
+        while (cur_item != NULL)
+        {
+            if (strcmp(key, cur_item -> pair.key) == 0)
+            {
+                return &cur_item -> pair;
+            }
+
+            cur_item = cur_item->next;
+        }
+    }
+    
+    //only gets here in case of no match
+    return NULL;
+}
