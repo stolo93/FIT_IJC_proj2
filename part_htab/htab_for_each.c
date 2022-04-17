09bda68f16709079233916a2d9c4d4a7221410dc
Samuel <samuelstolko@gmail.com>
2022-04-17 22:31:22 +0200

    added htab_for_each

---
+++ part_htab/htab_for_each.c
@@ -0,0 +1,32 @@
+/**
+ * @file htab_init.c
+ * @author Samuel Stolarik  (xstola03)
+ * @brief 2nd part of DU2 IJC
+ * 
+ * @date 2022-04-10
+ * 
+ */
+#include "htab.h"
+#include "p_htab.h"
+
+void htab_for_each(const htab_t * t, void (*f)(htab_pair_t *data))
+{
+    if (t == NULL || f == NULL){
+        return;
+    }
+
+    htab_item_t * cur;
+    for (size_t i = 0; i < t -> arr_size; i++)
+    {
+        cur = t -> ptr[i];
+
+        while (cur != NULL)
+        {
+            f(&cur ->pair);
+
+            cur = cur -> next;
+        }
+    }
+
+    return;
+}
