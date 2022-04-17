6ad4785a181349d7a3b7d1f6dd3a90bf0832f8c4
Samuel <samuelstolko@gmail.com>
2022-04-17 11:34:33 +0200

    hashing function

---
+++ part_htab/htab_hash_function.c
@@ -0,0 +1,19 @@
+/**
+ * @file htab_init.c
+ * @author Samuel Stolarik  (xstola03)
+ * @brief 2nd part of DU2 IJC
+ * 
+ * @date 2022-04-10
+ * 
+ */
+#include "htab.h"
+#include "p_htab.h"
+
+size_t htab_hash_function(const char *str)
+{
+    uint32_t h=0;     // musí mít 32 bitů
+    const unsigned char *p;
+    for(p=(const unsigned char*)str; *p!='\0'; p++)
+        h = 65599*h + *p;
+    return h;
+}
