a6fe484019daa4fd0508e0f6ffd0cd852524b403
Samuel <samuelstolko@gmail.com>
2022-04-17 11:50:11 +0200

    using hash function to get the starting index

--- part_htab/htab_find.c
+++ part_htab/htab_find.c
@@ -20,22 +20,21 @@ htab_pair_t * htab_find(htab_t * t, htab_key_t key)
         return NULL;
 
     htab_item_t * cur_item;
-    
-    for (int i = 0; i < t -> arr_size; i++)
+    size_t index = htab_hash_function(key) % t ->arr_size;
+
+    cur_item = t->ptr[index];
+
+    //walk the linked list starting at cur_item
+    while (cur_item != NULL)
     {
-        cur_item = t->ptr[i];
-
-        //walk the linked list starting at cur_item
-        while (cur_item != NULL)
+        if (strcmp(key, cur_item -> pair.key) == 0)
         {
-            if (strcmp(key, cur_item -> pair.key) == 0)
-            {
-                return &cur_item -> pair;
-            }
-
-            cur_item = cur_item->next;
+            return &cur_item -> pair;
         }
+
+        cur_item = cur_item->next;
     }
+
     
     //only gets here in case of no match
     return NULL;
